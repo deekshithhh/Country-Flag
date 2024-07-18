999ab5f6ff91312f9a553ab4c1e0057c79a1243e
Deekshith <deekshith3538@gmail.com>
2024-07-19 00:59:21 +0530

    Flags Added

---
+++ country-flag/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Countries from './countries';
+
+function App() {
+  return (
+    <div className="App">
+      <Countries/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ country-flag/src/countries.jsx
@@ -0,0 +1,74 @@
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { Typography } from '@mui/material';
+import styles from '../src/countries.module.css'
+import { useEffect,useState } from 'react';
+
+
+function CountryCard({ name, flag}){
+
+    console.log(name,flag)
+return(
+    <div>
+
+        <Card variant="outlined"  className={styles.CardImage} sx={{ maxWidth: 100 }}>
+        <CardMedia
+        component="img"
+        height="100px"
+        image={flag}
+        alt={"Flag of "+ name}
+      />
+      <Typography variant="body" color="text.primary">
+          {name}
+        </Typography>
+        </Card>
+    </div>
+)
+}
+
+
+function Countries(){
+const API_URL="https://xcountries-backend.azurewebsites.net/all"
+
+const[country,setCountry]=useState([])
+
+
+  useEffect(()=>{
+    const callcountries=async()=>{
+        try {
+            
+          const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+          const data = await response.json();
+         
+          setCountry(data)
+          return data;
+        } catch (error) {
+          console.error('Error fetching albums:', error);
+        }
+      }
+      callcountries()
+  },[])
+ 
+ console.log("API data",country)
+ //const temparr=[1,2,3,4,5]
+return (
+    <div style={{
+        display:"flex",
+        flexWrap:"wrap",
+         paddingLeft:"20px",
+        // paddingRight:"20px",
+         gap: "20px"
+    }}>
+       {/* temparr.map((ele)=>{
+        <Countries key={ele}/>
+       }) */}
+       {
+        country.map((ele) => <CountryCard  key ={ele.abbr} name={ele.name} flag={ele.flag} />)
+       }
+        
+       
+    </div>
+)
+}
+
+export default Countries
